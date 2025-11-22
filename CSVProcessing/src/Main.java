bf633c9cf1df5c6a65f48d7f5f08d86d8366f0c2
LucaMarks <lucamarks416@gmail.com>
2025-11-21 22:59:40 -0500

    HalfDone

---
+++ CSVProcessing/src/Main.java
@@ -0,0 +1,17 @@
+import java.io.File;
+import java.util.Scanner;
+
+public class Main {
+
+//    public static Scanner sc;
+
+    public static void main(String[] args){
+//try{sc = new Scanner(new File("resources.csv"));}catch(Exception e){}
+        new Panel();
+//        try {
+//            sc = new Scanner(new File("resources.csv"));
+//            System.out.println(sc.next());
+//        }catch (Exception e){}
+
+    }
+}
---
+++ CSVProcessing/src/Panel.java
@@ -0,0 +1,16 @@
+import Tiles.TileManager;
+
+public class Panel{
+
+    TileManager tileManager = new TileManager();
+
+
+    public Panel(){
+
+        //start by loading all of the tiles
+        loadTiles();
+    }
+
+    public void loadTiles(){
+    }
+}
